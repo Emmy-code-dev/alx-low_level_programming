b0060eaa68ba0f9c39841a87b5fdc3b6ac040db9
Emmy-code-dev <emmyronald@gmail.com>
2023-02-23 17:51:16 -0800

    Task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Positive_or_Negtaive
+ * Checks for positive or negative
+ * * Return: 0 always
+ */
+
+void postive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i);
+}
+
